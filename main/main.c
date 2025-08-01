4bd06ace50af42f128159db25372ee7d6f020be2
Vladyslav Lakei <tv42.lakei.vladyslav@gmail.com>
2025-08-01 10:15:04 +0300

    timer instantiated

--- main/main.c
+++ main/main.c
@@ -7,16 +7,17 @@
 #include "driver/uart.h"
 #include <esp_err.h>
 #include "driver/gpio.h"
+#include "esp_timer.h"
 
 uart_port_t UART_NUM = UART_NUM_2;
 
 void uart_init();
-void task(void *pvParameters);
+void main_task(void *pvParameters);
 
 void app_main(void)
 {   
     uart_init();
-    // xTaskCreate(task, "task", 1024, NULL, 1, NULL);
+    xTaskCreate(main_task, "task", 1024, NULL, 1, NULL);
 }
 
 void uart_init() {
@@ -25,6 +26,21 @@ void uart_init() {
     ESP_ERROR_CHECK(uart_driver_install(UART_NUM, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0));
 }
 
-void task(void *pvParameters) {
+void timer_callback(void *arg) {
     
+}
+
+void main_task(void *pvParameters) {
+    esp_timer_handle_t timer_handle;
+    esp_timer_create_args_t timer_config = {
+        .callback = NULL,
+        .arg = NULL,
+        .name = "read TX",
+        .dispatch_method = ESP_TIMER_ISR
+    };
+    ESP_ERROR_CHECK(esp_timer_create(&timer_config, &timer_handle));
+
+    while(1) {
+        vTaskDelay(10);
+    }
 }
